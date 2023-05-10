c2645fd1082fee3b3012ebe73fe4bb2f33df16af
Harsh Singh <121614660+harsyng@users.noreply.github.com>
2023-05-10 13:02:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
+  // Store email in localStorage for retrieval on the last page
+  localStorage.setItem('email', email);
+
+  // Redirect to another page
+  window.location.href = 'hello.html';
+});
